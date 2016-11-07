59f70ef9eeff566e29f99364981af8b7f274911f
ssh1 <ssh1@kgsoft.co.uk>
2016-11-07 11:40:36 +0000

    Experiment with var, val and println

--- hello.scala
+++ hello.scala
@@ -1,5 +1,14 @@
 object Hello {
    def main(args: Array[String]) {
-      println("Hello, Scala!")
+      var i = 10
+      val ival = "10"
+      var (v1, v2) = Pair(20, 30)
+      val (s1, s2, s3) = ("String1", "Stirng 2", "String 3")
+      println("Say \"Hello, Scala\" " + i + " times!")
+      println("Say \"Hello, Scala\" " + ival + " times!")
+      println("v1=" + v1 + "  v2=" + v2)
+      println("s1=" + s1 + "  s2=" + s2 + "  s3=" + s3)
+      v1=110;  v2=210
+      println("Changed v1=" + v1 + "  v2=" + v2)
    }
 }
